4cc97bcb7327f133d26fa1c8d873103ee06bc219
Krystian Booker <krystian.booker@gmail.com>
2025-11-22 21:52:55 -0500

    removed accidental push of git submodules

--- backend/third_party/allwpilib/thirdparty/imgui_suite/generated/fonts/src/imgui_DroidSans.cpp
+++
@@ -1,5 +0,0 @@
-#include "imgui_DroidSans.h"
-#include "DroidSans.inc"
-ImFont* ImGui::AddFontDroidSans(ImGuiIO& io, float size_pixels, const ImFontConfig* font_cfg, const ImWchar* glyph_ranges) {
-  return io.Fonts->AddFontFromMemoryCompressedTTF(DroidSans_compressed_data, DroidSans_compressed_size, size_pixels, font_cfg, glyph_ranges);
-}
--- backend/third_party/allwpilib/thirdparty/imgui_suite/generated/fonts/src/imgui_RobotoCondensedBold.cpp
+++
@@ -1,5 +0,0 @@
-#include "imgui_RobotoCondensedBold.h"
-#include "RobotoCondensedBold.inc"
-ImFont* ImGui::AddFontRobotoCondensedBold(ImGuiIO& io, float size_pixels, const ImFontConfig* font_cfg, const ImWchar* glyph_ranges) {
-  return io.Fonts->AddFontFromMemoryCompressedTTF(RobotoCondensedBold_compressed_data, RobotoCondensedBold_compressed_size, size_pixels, font_cfg, glyph_ranges);
-}
